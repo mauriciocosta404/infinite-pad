7be21d301febb6dbb1f514de97d84ba6b050792f
mcosta <mauricio.costa@xdsoftware.ao>
2024-12-12 16:48:17 +0100

    refactor: adding header structure

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isActive: boolean = false;
 
+  public handleActiveNavigation = () => {
+    this.isActive = !this.isActive;
+    console.log(this.isActive);
+  }
 }
