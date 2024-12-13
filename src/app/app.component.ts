a8b770023dd331a1bdd6f4642a6f80361a131cdc
mcosta <mauricio.costa@xdsoftware.ao>
2024-12-13 14:41:11 +0100

    feat: add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { HomeComponent } from "../view/home/home.component";
 import { AboutComponent } from "../view/about/about.component";
+import { AmbientPadComponent } from "../view/ambient-pad/ambient-pad.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HomeComponent, AboutComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, AboutComponent, AmbientPadComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
